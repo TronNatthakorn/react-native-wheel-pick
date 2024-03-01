c8d47f2aab097e4190cc65848d865375d0977ed6
Anthony Aubert <3479886+royraider@users.noreply.github.com>
2024-03-01 14:50:18 +0100

    Fix selectedValue android for Datepicker
    in case of timepicker, the date wasn't working, SelectedValue need to be a string for it to work.

--- src/picker.js
+++ src/picker.js
@@ -80,7 +80,7 @@ export default class Picker extends Component {
       <WheelCurvedPicker
         {...props}
         style={[styles.picker, style]}
-        selectedValue={this.state.selectedValue}
+        selectedValue={this.state.selectedValue+""}
         onValueChange={this.handleChange}
       >
         {pickerData.map((data, index) => (
